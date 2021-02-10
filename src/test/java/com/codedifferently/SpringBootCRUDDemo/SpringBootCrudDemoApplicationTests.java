fcb0f22421593b2eb256f29d45333cab82eb9e85
Jamal <jnasir247@gmail.com>
2021-02-09 19:34:02 -0500

    initial commit

---
+++ src/test/java/com/codedifferently/SpringBootCRUDDemo/SpringBootCrudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codedifferently.SpringBootCRUDDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
